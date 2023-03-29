d614e0ef761cb0e24adcbed39ff1f2adf02429d4
Charangl <chartrain.ang@gmail.com>
2023-03-29 12:02:46 +0200

    ajout props

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,27 @@ import './App.css'
 import MyPokemon from './components/PokemonCard'
 
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+const pokemon = pokemonList[1];
+
 function App() {
   return (
 <div>
-      <MyPokemon />
+      <MyPokemon pokemon={pokemon}/>
     </div>
   );
 }
 
 export default App;
+
+
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,17 +1,5 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
-
-  function MyPokemon(){
-    const pokemon = pokemonList[0];
+  function MyPokemon({pokemon}){
     return (
         <figure>
           {pokemon.imgSrc ? (
