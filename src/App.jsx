3f8efa05b5c1278fafb8f7885089f28b8a5cd30c
Charangl <chartrain.ang@gmail.com>
2023-03-30 12:28:06 +0200

    Partager un State

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MyPokemon from './components/PokemonCard'
+import NavBar from './components/NavBar'
 
 
 const pokemonList = [
@@ -33,29 +34,12 @@ const pokemonList = [
 
 function App() {
   const [pokemonIndex, setPokemonIndex] = useState(0);
-  const pokemonListLength = pokemonList.length;
-  const handleClickPrev = () => {
-    if (pokemonIndex > 0) {
-      setPokemonIndex(pokemonIndex - 1);
-    }
-  };
-  const handleClickNext = () => {
-    if (pokemonIndex < pokemonListLength - 1) {
-      setPokemonIndex(pokemonIndex + 1);
-    }
-  };
-
   return (<div>
       <MyPokemon pokemon={pokemonList[pokemonIndex]} />
-      <button onClick={handleClickPrev} hidden={pokemonIndex === 0}>
-        Précédent
-      </button>
-      <button
-        onClick={handleClickNext}
-        hidden={pokemonIndex === pokemonListLength - 1}
-      >
-        Suivant
-      </button></div>
+      <NavBar pokemonIndex={pokemonIndex}
+setPokemonIndex={setPokemonIndex}
+pokemonList={pokemonList}/>
+</div>
   );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+function NavBar({pokemonIndex, setPokemonIndex, pokemonList}){
+    const pokemonListLength = pokemonList.length;
+    const handleClickPrev = () => {
+      if (pokemonIndex > 0) {
+        setPokemonIndex(pokemonIndex - 1);
+      }
+    };
+    const handleClickNext = () => {
+      if (pokemonIndex < pokemonListLength - 1) {
+        setPokemonIndex(pokemonIndex + 1);
+      }
+    };
+    return (<div>
+        <button onClick={handleClickPrev} hidden={pokemonIndex === 0}>
+        {"Précédent"}
+      </button>
+      <button
+        onClick={handleClickNext}
+        hidden={pokemonIndex === pokemonListLength - 1}
+      >
+        {"Suivant"}
+      </button></div>
+      );
+    }
+
+
+    export default NavBar;
